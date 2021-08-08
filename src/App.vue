631ebed330ba08db7bc0631044d5d6846a551fb3
thisisazeez <imfostersyndrome@gmail.com>
2021-08-08 17:41:11 +0100

    first vue app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.jpg">
+    <HelloWorld msg="Welcome to Thisisazeez's first Vue.js App"/>
   </div>
 </template>
 
